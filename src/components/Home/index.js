82ff175adf8068129280b3e2beb1f69bee4291c6
hique023 <pedroviski10@hotmail.com>
2021-05-26 17:38:35 -0300

    Criado página Home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,23 @@
+// Global
+import React, { useEffect, useState } from 'react'
+import TopBar from '../TopBar'
+import ListPhases from '../ListPhases'
+// import firebase from '../../firebaseConfig.js'
+
+// Assets
+import './styles.css'
+
 export default function Home() {
+
+    const [data, setData] = useState(0)
+    const userUid = localStorage.getItem('userUid')
+    const nameUser = localStorage.getItem('nameUser')
+    // const db = firebase.firestore();
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="containerHome">
+            <TopBar name={nameUser} />
+            <ListPhases />
         </div>
     )
 }
