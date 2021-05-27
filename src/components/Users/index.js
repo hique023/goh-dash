13e377ed330c1b26f45ce00d7b7b1785e2741063
hique023 <pedroviski10@hotmail.com>
2021-05-26 23:31:16 -0300

    Inserido TopBarPhase na tela Users

--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -1,6 +1,18 @@
+// Global
+import React from 'react'
+import TopBarPhase from '../TopBarPhase'
+
+// Assets
+import './styles.css'
+
 export default function Users() {
+
+    const color = localStorage.getItem('color')
+    const nameUser = localStorage.getItem('nameUser')
+
     return (
-        <div>
+        <div className="containerUsers">
+            <TopBarPhase name={`Olá, ${nameUser}`} color={color} />
             <h1>Users</h1>
         </div>
     )
