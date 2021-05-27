7c2900e9727635846ae9bb7a144fcdf26fcb90b8
hique023 <pedroviski10@hotmail.com>
2021-05-26 23:22:02 -0300

    Criado tela Phase

--- src/components/Phase/index.js
+++ src/components/Phase/index.js
@@ -1,7 +1,34 @@
+// Global
+import React, { useEffect, useState } from 'react'
+import StageLink from '../StageLink'
+import TopBarPhase from '../TopBarPhase'
+import SubBar from '../SubBar'
+
+// Assets
+import './styles.css'
+
 export default function Phase() {
+
+    const color = localStorage.getItem('color')
+    const nameUser = localStorage.getItem('nameUser')
+    const phaseName = localStorage.getItem('phaseName')
+    const userUid = localStorage.getItem('userUid')
+
     return (
-        <div>
-            <h1>Phase</h1>
+        <div className="containerPhase">
+
+            <TopBarPhase name={`Olá, ${nameUser}`} color={color} />
+
+            {/* <SubBar name={phaseName} color={color} score={scoreSubBar} /> */}
+            <SubBar name={phaseName} color={color} />
+
+            <div className="listStage">
+                <StageLink name="Etapa 1" stageId="etapa1" color={color} />
+                <StageLink name="Etapa 2" stageId="etapa2" color={color} />
+                <StageLink name="Etapa 3" stageId="etapa3" color={color} />
+                <StageLink name="Etapa 4" stageId="etapa4" color={color} />
+            </div>
+
         </div>
     )
 }
