803baef1201ab8db09d21f1e1e2b895be0c26f09
hique023 <pedroviski10@hotmail.com>
2021-05-27 10:20:37 -0300

    Finalizado tela Users

--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -1,6 +1,7 @@
 // Global
 import React from 'react'
 import TopBarPhase from '../TopBarPhase'
+import SubBar from '../SubBar'
 
 // Assets
 import './styles.css'
@@ -9,11 +10,26 @@ export default function Users() {
 
     const color = localStorage.getItem('color')
     const nameUser = localStorage.getItem('nameUser')
+    const phaseName = localStorage.getItem('phaseName')
 
     return (
         <div className="containerUsers">
             <TopBarPhase name={`Olá, ${nameUser}`} color={color} />
-            <h1>Users</h1>
+
+            <SubBar name={phaseName} color={color} />
+
+            <div className="contentUsers">
+
+                <div className="users">
+                    <h1>Users</h1>
+                </div>
+
+                <div className="users">
+                    <h2>Users Managament</h2>
+                </div>
+
+            </div>
+
         </div>
     )
 }
