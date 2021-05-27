ce4798b78be89e67855e4cc158d050e254a3b54a
hique023 <pedroviski10@hotmail.com>
2021-05-26 23:34:20 -0300

    Inserido TopBarPhase na tela StageManagement

--- src/components/StageManagement/index.js
+++ src/components/StageManagement/index.js
@@ -1,6 +1,18 @@
+// Global
+import React from 'react'
+import TopBarPhase from '../TopBarPhase'
+
+// Assets
+import './styles.css'
+
 export default function StageManagement() {
+
+    const color = localStorage.getItem('color')
+    const nameUser = localStorage.getItem('nameUser')
+
     return (
-        <div>
+        <div className="containerStageManagement">
+            <TopBarPhase name={`Olá, ${nameUser}`} color={color} />
             <h1>Stage Management</h1>
         </div>
     )
