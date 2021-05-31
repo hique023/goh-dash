3b3d3f177020cb88d8641505fa5e4aea934cbfb3
hique023 <pedroviski10@hotmail.com>
2021-05-30 22:51:13 -0300

    Criado componente ListUser e página EditUser

---
+++ src/components/EditUser/index.js
@@ -0,0 +1,13 @@
+// Global
+import React from 'react'
+
+// Assets
+import 'styles.css'
+
+export default function EditUser() {
+    return (
+        <div className="containerEditUser">
+            <h1>Edit User</h1>
+        </div>
+    )
+}
---
+++ src/components/ListUser/index.js
@@ -0,0 +1,13 @@
+// Global
+import React from 'react'
+
+// Assets
+import 'styles.css'
+
+export default function ListUser() {
+    return (
+        <div className="containerListUser">
+            <h1>List User</h1>
+        </div>
+    )
+}
