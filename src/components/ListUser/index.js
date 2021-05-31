10a4aae47c068acf2b362cceac26248230eb1a36
hique023 <pedroviski10@hotmail.com>
2021-05-30 23:55:44 -0300

    Recuperado lista de usuários e criado método para edição

--- src/components/ListUser/index.js
+++ src/components/ListUser/index.js
@@ -7,18 +7,19 @@ import "./styles.css";
 import { FiEdit } from "react-icons/fi";
 
 export default function ListUser(props) {
+  function editUser() {
+    localStorage.setItem("idUser", props.uid);
+  }
+
   return (
     <div className="containerListUser">
       <div className="contentListUser">
-        <img
-          src="https://firebasestorage.googleapis.com/v0/b/goh-global-41575.appspot.com/o/herois%2FAdmin.png?alt=media&token=690b1f4d-7b52-4984-a23a-9c92fa83e4fe"
-          alt="Imagem de Perfil"
-        />
+        <img src={props.avatar} alt="Imagem de Perfil" />
 
         <h1>{props.name}</h1>
       </div>
 
-      <button type="button" className="editButton">
+      <button type="button" className="editButton" onClick={editUser}>
         <Link to="edituser">
           <FiEdit size={20} color="var(--edit)" />
         </Link>
--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -1,8 +1,9 @@
 // Global
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TopBarPhase from "../TopBarPhase";
 import SubBar from "../SubBar";
 import ListUser from "../ListUser";
+import firebase from "../../firebaseConfig";
 
 // Assets
 import "./styles.css";
@@ -11,20 +12,55 @@ export default function Users() {
   const color = localStorage.getItem("color");
   const nameUser = localStorage.getItem("nameUser");
   const phaseName = localStorage.getItem("phaseName");
+  const db = firebase.firestore();
+
+  const [user, setUser] = useState({ data: [] });
+
+  function getuser() {
+    const count = [];
+
+    db.collection("users")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          count.push(doc.data());
+        });
+
+        count.sort(function (a, b) {
+          if (a.user > b.user) {
+            return -1;
+          } else {
+            return true;
+          }
+        });
+
+        setUser({ data: count });
+      });
+  }
+
+  useEffect(() => {
+    getuser();
+  }, []);
 
   return (
-    <div className="containerUsers">
-      <TopBarPhase name={`Olá, ${nameUser}`} color={color} />
+    user && (
+      <div className="containerUsers">
+        <TopBarPhase name={`Olá, ${nameUser}`} color={color} />
 
-      <SubBar name={phaseName} color={color} />
+        <SubBar name={phaseName} color={color} />
 
-      <div className="contentUsers">
-        <ListUser name="Usuário 01" />
-        <ListUser name="Usuário 02" />
-        <ListUser name="Usuário 03" />
-        <ListUser name="Usuário 04" />
-        <ListUser name="Usuário 05" />
+        <div className="contentUsers">
+          {user.data.map((item, key) => (
+            <div key={key}>
+              <ListUser
+                name={item.firstName}
+                avatar={item.avatar}
+                uid={item.uid}
+              />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    )
   );
 }
