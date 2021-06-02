5fa5be1e76dedb310cf6deb4f0e99f8c38de9b37
hique023 <pedroviski10@hotmail.com>
2021-06-02 20:36:54 -0300

    Recuperado questões na tela Stage

--- src/components/LisQuestion/index.js
+++ src/components/LisQuestion/index.js
@@ -6,7 +6,15 @@ import { Link } from "react-router-dom";
 import "./styles.css";
 import { FiEdit } from "react-icons/fi";
 
-export default function ListQuestion(props) {
+export default function ListQuestion({
+  question,
+  answer1,
+  answer2,
+  answer3,
+  answer4,
+  correctAnswer,
+  scoreQuestion,
+}) {
   function editQuestion() {
     alert("Edição de pergunta");
   }
@@ -16,40 +24,37 @@ export default function ListQuestion(props) {
       <div className="contentListQuestion">
         <div className="slotQuestion">
           <h1>Enunciado:</h1>
-          <h2>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-            incidunt id enim unde veniam?
-          </h2>
+          <h2>{question}</h2>
         </div>
         <hr />
         <div className="slotQuestion">
           <h1>Reposta 1:</h1>
-          <h2>Cappta</h2>
+          <h2>{answer1}</h2>
         </div>
         <hr />
         <div className="slotQuestion">
           <h1>Reposta 2:</h1>
-          <h2>Cappta</h2>
+          <h2>{answer2}</h2>
         </div>
         <hr />
         <div className="slotQuestion">
           <h1>Reposta 3:</h1>
-          <h2>Cappta</h2>
+          <h2>{answer3}</h2>
         </div>
         <hr />
         <div className="slotQuestion">
           <h1>Reposta 4:</h1>
-          <h2>Cappta</h2>
+          <h2>{answer4}</h2>
         </div>
         <hr />
         <div className="slotQuestion">
           <h1>Reposta Correta:</h1>
-          <h2>Cappta</h2>
+          <h2>{correctAnswer}</h2>
         </div>
         <hr />
         <div className="slotQuestion">
           <h1>Pontuação:</h1>
-          <h2>6</h2>
+          <h2>{scoreQuestion}</h2>
         </div>
       </div>
 
--- src/components/Stage/index.js
+++ src/components/Stage/index.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import TopBarPhase from "../TopBarPhase";
 import SubBar from "../SubBar";
 import ListQuestion from "../LisQuestion";
+import firebase from "../../firebaseConfig";
 
 // Assets
 import "./styles.css";
@@ -16,6 +17,40 @@ export default function Stage(props) {
   const stageName = localStorage.getItem("subBarName");
   const userUid = localStorage.getItem("userUid");
 
+  const db = firebase.firestore();
+
+  const [questions, setQuestions] = useState();
+
+  function getQuestions() {
+    const count = [];
+
+    var docRef = db.collection(phaseId).doc(stageId);
+
+    docRef
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          const dataDb = doc.data();
+          // setData(data);
+
+          console.log("====================================");
+          console.log(dataDb);
+          setQuestions(dataDb);
+          console.log("====================================");
+        } else {
+          // doc.data() will be undefined in this case
+          console.log("No such document!");
+        }
+      })
+      .catch((error) => {
+        console.log("Error getting document:", error);
+      });
+  }
+
+  useEffect(() => {
+    getQuestions();
+  }, []);
+
   return (
     <div className="containerStage">
       <TopBarPhase name={`Olá, ${nameUser}`} color={color} />
@@ -27,9 +62,6 @@ export default function Stage(props) {
       </Link>
 
       <div className="listStage">
-        <ListQuestion />
-        <ListQuestion />
-        <ListQuestion />
         <ListQuestion />
         <ListQuestion />
       </div>
